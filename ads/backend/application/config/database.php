a8a62074e19ee7f56af13380137127575e800d2f
Jairus Bondoc <jairus@nmgresources.ph>
2012-10-04 14:23:11 +0800

    added municipality config for ads

--- ads/backend/application/config/database.php
+++ ads/backend/application/config/database.php
@@ -63,6 +63,7 @@ $db['default']['dbcollat'] = 'utf8_general_ci';
 $db['default']['swap_pre'] = '';
 $db['default']['autoinit'] = TRUE;
 $db['default']['stricton'] = FALSE;
+$_SESSION['municipality'] = "ads";
 
 
 /* End of file database.php */
